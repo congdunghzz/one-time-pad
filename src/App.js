cf465d33be47fa85b81efc8052553b1b4bc272ca
congdunghzz <lebaoduy023502@gmail.com>
2024-03-24 00:33:25 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
+import Solution from './components/Solution';
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [text, setText] = useState('');
+
+  const handleBtnClick = () => {
+    setCards(prev => ([...prev, text]));
+    setText('');
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-5">
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroup-sizing-default">Plaintext</span>
+        </div>
+        <input type="text" className="form-control" aria-label="Sizing example input" value={text}
+        onChange={(e)=>{setText(e.target.value)}}/>
+      </div>
+      <button type="button" className="btn btn-primary" onClick={handleBtnClick}>Encrypt</button>
+
+      <div className="row mt-5">
+        {
+          cards.map(card => ( 
+            <Solution plaintext={card}/>
+
+          ))
+        }
+      </div>
     </div>
   );
 }
